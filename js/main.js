0575755f209fd4c666a5ca74251de1c50198b341
José Marinho <68341332+clowdcap@users.noreply.github.com>
2021-03-16 23:38:20 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+var variavel = document.querySelector('');
+var variavel = document.querySelectorAll('');
+
+variavel.addEventListener('click', function(){
+
+});
+
+
+
+var span = document.querySelector('.conteudo span');
+span.addEventListener('click', function(){
+    var conteudo = document.querySelector('.conteudo p');
+    if(conteudo.classList.contains('mostrar')){
+        //alert('Esconda  o contecuo');
+        span.innerHTML = 'Ver mais'
+        conteudo.classList.remove('mostrar');
+    } else {
+        span.innerHTML = 'Ocultar'
+        conteudo.classList.add('mostrar');
+        //alert('mostre o conteudo');
+    }
+});
+
+
+Movimento 
+
+
+var box = document.querySelectorAll('.box')[0];
+    box.addEventListener('click', function(){
+        box.classList.add('animEsquerda');
+    }, 1000);
+*/
